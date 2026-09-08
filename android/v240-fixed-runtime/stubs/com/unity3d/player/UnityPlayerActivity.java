1a431111f9fde7520265ce58fc9cb3907e5ae263
hoonex <163626004+hoonex@users.noreply.github.com>
2026-09-09 01:31:50 +0900

    Add compile-only Unity activity stub for 2.4 runtime

---
+++ android/v240-fixed-runtime/stubs/com/unity3d/player/UnityPlayerActivity.java
@@ -0,0 +1,7 @@
+package com.unity3d.player;
+
+import android.app.Activity;
+
+/** Compile-only stub. This class is removed before DEX packaging. */
+public class UnityPlayerActivity extends Activity {
+}
